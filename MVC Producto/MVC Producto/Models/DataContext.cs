0bdb278e57e055c2b40c7d14d359726ab6815444
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-04-24 22:08:33 -0400

    Commit MVC Producto

---
+++ MVC Producto/MVC Producto/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Producto.Models
+{
+    public class DataContext:DbContext 
+
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVC_Producto.Models.Producto> Productoes { get; set; }
+    }
+}
---
+++ MVC Producto/MVC Producto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Producto.Startup))]
+namespace MVC_Producto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
